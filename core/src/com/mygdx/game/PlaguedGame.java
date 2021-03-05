9bc1d390a7f7b662ee8f576d37fbee3b06a89df1
David Ulloa <dulloa6310@gmail.com>
2021-03-05 18:20:55 -0500

    Changed to Game interface and Screen classes.

--- core/src/com/mygdx/game/PlaguedGame.java
+++ core/src/com/mygdx/game/PlaguedGame.java
@@ -1,39 +1,35 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.mygdx.game.states.GameStateManager;
-import com.mygdx.game.states.PlayState;
+import com.mygdx.game.screens.PlayScreen;
 
-public class PlaguedGame extends ApplicationAdapter {
+public class PlaguedGame extends Game {
 
 	public static final int WIDTH = 800;
 	public static final int HEIGHT = 800;
 	public static final String TITLE = "Plagued";
 
-	private GameStateManager gsm;
-	private SpriteBatch batch;
+	public SpriteBatch batch;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		gsm = new GameStateManager();
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		gsm.push(new PlayState(gsm));
+		setScreen(new PlayScreen(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		gsm.update(Gdx.graphics.getDeltaTime());
-		gsm.render(batch);
+		super.render();
 	}
 
 	@Override
 	public void dispose () {
 		batch.dispose();
 	}
+
 }
---
+++ core/src/com/mygdx/game/screens/PlayScreen.java
@@ -0,0 +1,74 @@
+package com.mygdx.game.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.mygdx.game.PlaguedGame;
+
+public class PlayScreen implements Screen {
+
+    private PlaguedGame game;
+    private OrthographicCamera gameCam;
+    private Viewport gamePort;
+
+    private Texture labFloorOne = new Texture("labFloorOne.png");
+    private Texture gunnerModel = new Texture("gunnerModel.png");
+
+    public PlayScreen(PlaguedGame game) {
+        this.game = game;
+        gameCam = new OrthographicCamera();
+        gamePort = new FitViewport(PlaguedGame.WIDTH, PlaguedGame.HEIGHT, gameCam);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.setProjectionMatrix(gameCam.combined);
+
+        game.batch.begin();
+
+        for (int x = 0; x < PlaguedGame.WIDTH / labFloorOne.getWidth(); x++) {
+            for (int y = 0; y < PlaguedGame.HEIGHT / labFloorOne.getHeight(); y++) {
+                game.batch.draw(labFloorOne, x * labFloorOne.getWidth(), y * labFloorOne.getHeight());
+            }
+        }
+
+
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        gamePort.update(width, height);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
